12afb9446170498d57ff91b7eff39727c8a3e46c
suvadeep <suvadeepm07092@gmail.com>
2026-05-23 17:40:11 +0530

    Add noteson functions

--- playground/variables.ts
+++ playground/variables.ts
@@ -1,1 +1,12 @@
-let movieTitle :string ="Amadeus"
+// A function that doesn't finish running 
+const neverStop =():never => {
+    while(true){
+        console.log("I'm still going!");
+        
+    }
+}
+
+//A function that throws an exception
+const giveError =(msg:string)=>{
+    throw new Error(msg)
+}

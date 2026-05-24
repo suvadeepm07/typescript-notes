03a91e66dc526bb9fabdace63c1efe66c5dd8a00
suvadeep <suvadeepm07092@gmail.com>
2026-05-24 20:10:47 +0530

    Add notes on tuples and enums

--- playground/variables.ts
+++ playground/variables.ts
@@ -1,12 +1,35 @@
-//An interface
-interface Person{
-    name:string;
-    age:number;
+//Numeric Enums 
+enum Responses1 {
+    n0, //0
+    yes , //2
+    maybe, //3
+
 }
 
-//Use the type alias in the annotation
-const sayHappyBirthday =(person:Person)=>{
-    return `Hey ${person.name} , congrats on turning ${person.age}`
+enum Responses2 {
+    no = 2, //2
+    yes, //3
+    maybe , //4
+
 }
 
-sayHappyBirthday({name:"Jerry",age:42})
+enum Responses3 {
+    no =2, //2
+    yes = 10, //10
+    maybe = 24 , //24
+}
+
+//String Enums 
+enum Responses4{
+    no ="No",
+    yes ='Yes',
+    maybe ='Maybe'
+}
+
+//Heterogenous Enums 
+enum Responses5{
+    no = 0,
+    yes = 1,
+    maybe ="Maybe"
+}
+

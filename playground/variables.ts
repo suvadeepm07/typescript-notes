8a9172a274b95c05255df6de1d9dcb459d58b039
suvadeep <suvadeepm07092@gmail.com>
2026-05-24 08:59:08 +0530

    Add notes on enums

--- playground/variables.ts
+++ playground/variables.ts
@@ -1,11 +1,32 @@
-//using brackets
-//string array
-let names :string[]=["hello","world"]
-//number array
-let ages :number[]= [24,32,19,29]
+//Numeric Enums
+enum Responses1 {
+    no,//0
+    yes,//1
+    maybe,//2
+}
 
-//An alternate syntax
-//string array
-let names2:Array<string>=["hello", "world"]
-//number array
-let ages2 :Array<number> =[24,32,19,29]
+enum Response2{
+    no = 2, //2
+    yes , //3
+    maybe , //4
+}
+
+enum Responses3 {
+    no = 2 ,//2
+    yes = 10 , //10
+    maybe = 24, //24
+}
+
+//String Enums 
+enum Responses4{
+    no ="No",
+    yes ="Yes",
+    maybe ="Maybe"
+}
+
+//Heterogenous Enums
+enum Responses5{
+    no = 0,
+    yes = 1,
+    maybe ="Maybe"
+}
